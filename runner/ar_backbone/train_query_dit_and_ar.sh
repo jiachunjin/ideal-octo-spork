c7ab137f164e65a232e91ff47676e6e7c215447d
Jiachun Jin <jiachun22.jin@gmail.com>
2025-07-08 11:33:02 +0000

    sync

---
+++ runner/ar_backbone/train_query_dit_and_ar.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch \
+--config_file config/ddp/deepspeed \
+--main_process_port 30002 \
+--num_processes 8 \
+runner/ar_backbone/train_query_dit.py \
+--config config/ar_backbone/query_dit_train_ar.yaml
