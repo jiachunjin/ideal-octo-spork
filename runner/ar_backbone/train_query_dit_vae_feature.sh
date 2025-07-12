a11c677df0158ed392cb4f232d639b2837b07b8a
Jiachun Jin <jiachun22.jin@gmail.com>
2025-07-12 07:06:54 +0000

    sync train_query_dit_vae_feature.sh

--- runner/ar_backbone/train_query_dit_vae_feature.sh
+++ runner/ar_backbone/train_query_dit_vae_feature.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch \
 --main_process_port 30002 \
 --num_processes 8 \
 runner/ar_backbone/train_query_dit.py \
---config config/ar_backbone/query_dit_train_ar.yaml
+--config config/ar_backbone/query_dit_vae_feature.yaml
