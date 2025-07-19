2bc8b14b7a45ea33d161b9cdd51513d1348907dd
Jiachun Jin <jiachun22.jin@gmail.com>
2025-07-19 06:25:50 +0000

    sync

--- multi_node_run.sh
+++ multi_node_run.sh
@@ -1,6 +1,10 @@
 WORLD_SIZE=2
 SHARED_DIR="/data/phd/jinjiachun/shared_dir"
 
+ACCELERATE_CONFIG_FILE="config/accelerate_config/multi_node_example_zero2.yaml"
+MAIN_PYTHON_SCRIPT="runner/ar_backbone/train_diffhead.py"
+MAIN_PYTHON_SCRIPT_ARGS="config/ar_backbone/diffhead_and_ar.yaml"
+
 # 解析命令行参数
 for ARG in "$@"; do
   case $ARG in
@@ -61,6 +65,19 @@ fi
 # 所有节点读取 master 地址和端口
 read MASTER_ADDR MASTER_PORT < $MASTER_FILE
 
-echo "[INFO] Starting torchrun with world size $WORLD_SIZE, gpus per node $NUM_GPUS_PER_NODE, master_addr=$MASTER_ADDR, master_port=$MASTER_PORT, node_rank=$NODE_RANK"
-echo "[INFO] Using config: $CONFIG_PATH"
-echo "[INFO] Shared directory: $SHARED_DIR"
+# echo "[INFO] Starting accelerate with world size $WORLD_SIZE, gpus per node $NUM_GPUS_PER_NODE, master_addr=$MASTER_ADDR, master_port=$MASTER_PORT, node_rank=$NODE_RANK"
+# echo "[INFO] Using config: $CONFIG_PATH"
+# echo "[INFO] Shared directory: $SHARED_DIR"
+
+NUM_PROCESSES=$((NUM_GPUS_PER_NODE * WORLD_SIZE))
+echo "[INFO] Total processes to launch: $NUM_PROCESSES (calculated from ${NUM_GPUS_PER_NODE} GPUs/node * ${WORLD_SIZE} machines)"
+
+echo "[INFO] Starting accelerate launch with the following parameters:"
+echo "  --config_file ${ACCELERATE_CONFIG_FILE}"
+echo "  --main_process_ip ${MASTER_ADDR}"
+echo "  --main_process_port ${MASTER_PORT}"
+echo "  --machine_rank ${NODE_RANK}"
+echo "  --num_machines ${WORLD_SIZE}"
+echo "  --num_processes ${NUM_PROCESSES}"
+echo "  Python Script: ${MAIN_PYTHON_SCRIPT}"
+echo "  Script Arguments: ${MAIN_PYTHON_SCRIPT_ARGS}"
