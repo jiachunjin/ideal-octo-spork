18796cf76f24b01475c5ae2b345fac533f0408bc
Jiachun Jin <jiachun22.jin@gmail.com>
2025-07-29 06:48:47 +0000

    sync

--- runner/ar_backbone/hybrid_test.py
+++ runner/ar_backbone/hybrid_test.py
@@ -183,35 +183,31 @@ def main():
 
     prompts = [
         "A stunning princess from kabul in red, white traditional clothing, blue eyes, brown hair",
-        "A stunning princess from kabul in red, white traditional clothing, blue eyes, brown hair",
-        "A stunning princess from kabul in red, white traditional clothing, blue eyes, brown hair",
+        "A soft, natural portrait photograph captures a young woman with fair skin and long, ash-blonde hair cascading gently over her shoulders. At the very bottom of the frame, in simple, elegant lettering, appears the phrase 'BE KIND'",
+        "The image depicts a modern, multi-story building with a white facade and numerous balconies. The structure is partially covered in purple scaffolding on the right side, indicating ongoing construction or renovation. The building is situated in an urban area with clear blue skies above. In front of the building, there is a paved plaza with some greenery and a few palm trees. A street lamp stands prominently on the left side of the plaza. To the right, part of another building with a beige exterior is visible. The scene suggests a sunny day in a developed cityscape.",
+        "A photo of 4 TVs in a row, with a white background",
+        "The image depicts the American Red Cross building, characterized by its neoclassical architectural style. The structure features tall, white columns supporting a pediment and a balustrade at the top. The facade is adorned with large windows, some of which have red crosses, symbolizing the organization's humanitarian mission. The building is set against a clear blue sky, with a tree partially obscuring the right side of the image. The overall appearance suggests a sense of stability and dedication to service, reflecting the Red Cross's commitment to aid and support.",
+        "A photo of a red dog",
+        "Scientist at Sunway University conducts research in a laboratory setting.",
+        "A serious Santa Claus in a rustic setting.",
+        "Muscular man in workout attire, standing confidently by a railing.",
+        "Confident man in leather jacket leaning against a wall.",
     ]
     cfg_scale = 3
-    for img_idx, prompt in enumerate(prompts):
-        conversation = [
-            {
-                "role": "<|User|>",
-                "content": prompt,
-            },
-            {"role": "<|Assistant|>", "content": ""},
-        ]
-        sft_format = vl_chat_processor.apply_sft_template_for_multi_turn_prompts(
-            conversations = conversation,
-            sft_format    = vl_chat_processor.sft_format,
-            system_prompt = "",
-        )
-        prompt = sft_format + vl_chat_processor.image_start_tag
+
+    for img_idx, prompt in enumerate(prompts): 
         input_ids = tokenizer.encode(prompt)
-        input_ids = torch.LongTensor(input_ids).to(device)
+        input_ids = torch.LongTensor(input_ids)
+        input_ids = torch.cat([input_ids, torch.tensor([100003])]).to(device)
 
         if cfg_scale > 1:
             input_ids = input_ids.repeat(2, 1)
-            input_ids[1, :] = tokenizer.pad_token_id
-            text_embedding = janus.language_model.get_input_embeddings()(input_ids).to(device, dtype)
+            input_ids[1, :-1] = 100002
+            text_embedding = janus.language_model.get_input_embeddings()(input_ids).to(device)
         else:
-            text_embedding = janus.language_model.get_input_embeddings()(input_ids).to(device, dtype)
+            text_embedding = janus.language_model.get_input_embeddings()(input_ids).to(device)
 
-        generated_tokens = torch.zeros((1, 576, 16)).to(device, dtype)
+        generated_tokens = torch.zeros((1, 576, 16)).to(device)
 
         for i in trange(576):
             outputs = janus.language_model.model(inputs_embeds=text_embedding, use_cache=True, past_key_values=outputs.past_key_values if i != 0 else None)
