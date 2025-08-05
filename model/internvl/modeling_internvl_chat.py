14a5283d7bd7e7bc8e0a8146fbce8956feb36cea
Jiachun Jin <jiachun22.jin@gmail.com>
2025-08-05 15:25:10 +0000

    sync

--- model/internvl/modeling_internvl_chat.py
+++ model/internvl/modeling_internvl_chat.py
@@ -286,6 +286,8 @@ class InternVLChatModel(PreTrainedModel):
 
         model_inputs = tokenizer(query, return_tensors='pt')
         input_ids = model_inputs['input_ids'].to(self.device)
+        print(tokenizer.decode(input_ids[0], skip_special_tokens=False))
+        exit(0)
         attention_mask = model_inputs['attention_mask'].to(self.device)
         generation_config['eos_token_id'] = eos_token_id
         generation_output = self.generate(
