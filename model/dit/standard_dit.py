4a5597592c745bd25c70274cf69ff1246269f469
Jiachun Jin <jiachun22.jin@gmail.com>
2025-08-14 08:19:28 +0000

    sync

---
+++ model/dit/standard_dit.py
@@ -0,0 +1,225 @@
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+from timm.models.vision_transformer import Attention, Mlp
+
+
+def modulate(x, shift, scale):
+    return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
+
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations.
+    """
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+class LabelEmbedder(nn.Module):
+    """
+    The dropout is only down by the root transformer
+    Embeds class labels into vector representations. Also handles label dropout for classifier-free guidance.
+    """
+    def __init__(self, num_classes, hidden_size):
+        super().__init__()
+        self.embedding_table = nn.Embedding(num_classes + 1, hidden_size)
+        self.num_classes = num_classes
+
+    def forward(self, labels):
+        embeddings = self.embedding_table(labels)
+        return embeddings
+
+def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False, extra_tokens=0):
+    """
+    grid_size: int of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    """
+    grid_h = np.arange(grid_size, dtype=np.float32)
+    grid_w = np.arange(grid_size, dtype=np.float32)
+    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([2, 1, grid_size, grid_size])
+    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+    if cls_token and extra_tokens > 0:
+        pos_embed = np.concatenate([np.zeros([extra_tokens, embed_dim]), pos_embed], axis=0)
+    return pos_embed
+
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1) # (H*W, D)
+    return emb
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float64)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out) # (M, D/2)
+    emb_cos = np.cos(out) # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+class DiTBlock(nn.Module):
+    """
+    A DiT block with adaptive layer norm zero (adaLN-Zero) conditioning.
+    """
+    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0, **block_kwargs):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.attn = Attention(hidden_size, num_heads=num_heads, qkv_bias=True, **block_kwargs)
+        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        mlp_hidden_dim = int(hidden_size * mlp_ratio)
+        approx_gelu = lambda: nn.GELU(approximate="tanh")
+        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 6 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, c):
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
+        x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa))
+        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))
+        return x
+
+class FinalLayer(nn.Module):
+    """
+    The final layer of DiT.
+    """
+    def __init__(self, hidden_size, out_channels):
+        '''
+        produce_condition: whether the current transformer should produce output with the same shape as the input or produce a condition for the next transformer
+        '''
+        super().__init__()
+        self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(hidden_size, out_channels, bias=True)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 2 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, c):
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        x = modulate(self.norm_final(x), shift, scale)
+        x = self.linear(x)
+        return x
+
+class DiT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.x_embedder = nn.Linear(config.in_channels, config.hidden_size)
+        self.t_embedder = TimestepEmbedder(config.hidden_size)
+        self.y_embedder = LabelEmbedder(config.num_classes, config.hidden_size)
+        self.pos_embed = nn.Parameter(torch.zeros(1, config.num_tokens, config.hidden_size), requires_grad=False)
+        self.blocks = nn.ModuleList([
+            DiTBlock(config.hidden_size, config.num_heads, mlp_ratio=4) for _ in range(config.depth)
+        ])
+        self.final_layer = FinalLayer(config.hidden_size, config.out_channels)
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+        
+        # Initialize (and freeze) pos_embed by sin-cos embedding:
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.config.num_tokens ** 0.5))
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))       
+        # Initialize label embedding table:
+        nn.init.normal_(self.y_embedder.embedding_table.weight, std=0.02)
+        # Initialize timestep embedding MLP:
+        nn.init.normal_(self.t_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.t_embedder.mlp[2].weight, std=0.02)
+        # Zero-out adaLN modulation layers in DiT blocks:
+        for block in self.blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+        # Zero-out output layers:
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+
+    def forward(self, x, t, y):
+        x = self.x_embedder(x) + self.pos_embed
+        t_embed = self.t_embedder(t)
+        y_embed = self.y_embedder(y)
+        c = t_embed + y_embed
+        for block in self.blocks:
+            x = block(x, c)
+        x = self.final_layer(x, c)
+
+        return x
+
+
+if __name__ == "__main__":
+    from omegaconf import OmegaConf
+    
+    config = OmegaConf.load("config/clip_dit/clip_dit.yaml")
+    model = DiT(config.dit)
+    num_params = sum(p.numel() for p in model.parameters())
+    print(f"Number of parameters: {num_params / 1e6:.2f}M")
+
+    B = 3
+    x = torch.randn(B, 1024, 1024)
+    t = torch.randint(0, 1000, (B,))
+    y = torch.randint(0, 1000, (B,))
+    out = model(x, t, y)
+    print(out.shape)
+    
--- runner/internvl/sandwich.py
+++ runner/internvl/sandwich.py
@@ -6,6 +6,7 @@ import torch
 from transformers import Qwen2Model
 from transformers.models.qwen2.modeling_qwen2 import Qwen2DecoderLayer
 from model.internvl.modeling_internvl_chat import InternVLChatModel
+from diffusers import DDPMScheduler
 
 def add_hat_to_intern(
     model: Qwen2Model,
@@ -27,32 +28,100 @@ def add_hat_to_intern(
 
     return model
 
-def equip_internvl(ar_model, num_hat):
-    ar_model.language_model.model = add_hat_to_intern(ar_model.language_model.model, num_hat)
+def equip_internvl(ar_model, config):
+    ar_model.language_model.model = add_hat_to_intern(ar_model.language_model.model, config.num_hat)
     current_num_layers = len(ar_model.language_model.model.layers)
-    new_layer_indices = range(current_num_layers - num_hat, current_num_layers)
+    new_layer_indices = range(current_num_layers - config.num_hat, current_num_layers)
 
     ar_model.requires_grad_(False)
     for idx in new_layer_indices:
         layer = ar_model.language_model.model.layers[idx]
         layer.requires_grad_(True)
+
+    # diff_head = SimpleMLPAdaLN(
+    #     in_channels    = config.diffhead.x_dim,
+    #     model_channels = config.diffhead.hidden_size,
+    #     out_channels   = config.diffhead.x_dim,
+    #     z_channels     = config.diffhead.z_dim,
+    #     num_res_blocks = config.diffhead.depth,
+    # )
+    train_scheduler = DDPMScheduler(
+        beta_schedule          = "scaled_linear",
+        beta_start             = 0.00085,
+        beta_end               = 0.012,
+        num_train_timesteps    = 1000,
+        clip_sample            = False,
+        prediction_type        = "v_prediction",
+        steps_offset           = 1,
+        trained_betas          = None,
+        timestep_spacing       = "trailing",
+        rescale_betas_zero_snr = True
+    )
     
-    return ar_model
+    return ar_model, train_scheduler
 
-def dev():
-    device = torch.device("cuda:0")
-    intern_vl_1b_path = "/data/phd/jinjiachun/ckpt/OpenGVLab/InternVL3-1B"
+def get_accelerator(config):
+    output_dir = os.path.join(config.root, config.exp_name, config.output_dir)
+    os.makedirs(output_dir, exist_ok=True)
+    logging_dir = os.path.join(output_dir, config.logging_dir)
+    project_config = ProjectConfiguration(project_dir=config.output_dir, logging_dir=logging_dir)
+    accelerator = Accelerator(
+        log_with                    = None if config.report_to == "no" else config.report_to,
+        mixed_precision             = config.mixed_precision,
+        project_config              = project_config,
+        gradient_accumulation_steps = config.gradient_accumulation_steps,
+    )
 
-    ar_model = InternVLChatModel.from_pretrained(intern_vl_1b_path)
-    ar_model = equip_internvl(ar_model, 8)
+    return accelerator, output_dir
+# def dev():
+#     device = torch.device("cuda:0")
+#     intern_vl_1b_path = "/data/phd/jinjiachun/ckpt/OpenGVLab/InternVL3-1B"
 
-    print(ar_model)
+#     ar_model = InternVLChatModel.from_pretrained(intern_vl_1b_path)
+#     ar_model = equip_internvl(ar_model, 8)
+
+#     print(ar_model)
+import torch
+import pprint
+import argparse
+from tqdm import tqdm
+from einops import rearrange
+from omegaconf import OmegaConf
+from accelerate import Accelerator
+from accelerate.state import AcceleratorState
+from accelerate.utils import ProjectConfiguration
+
+from util.misc import process_pretrained_model_path
+
+
+def main(args):
+    config = OmegaConf.load(args.config)
+    config = process_pretrained_model_path(config)
+
+    accelerator, output_dir = get_accelerator(config.train)
+    accelerator.print("Configuration:")
+    accelerator.print(pprint.pformat(OmegaConf.to_container(config, resolve=True), indent=2, width=120).strip('{}'))
+    AcceleratorState().deepspeed_plugin.deepspeed_config['train_micro_batch_size_per_gpu'] = config.data.batch_size
+    accelerator.print(AcceleratorState().deepspeed_plugin.deepspeed_config)
+
+    if config.base_model == "intern_vl_1b":
+        ar_model = InternVLChatModel.from_pretrained(config.intern_vl_1b_path)
+    elif config.base_model == "intern_vl_2b":
+        ar_model = InternVLChatModel.from_pretrained(config.intern_vl_2b_path)
+    elif config.base_model == "intern_vl_8b":
+        ar_model = InternVLChatModel.from_pretrained(config.intern_vl_8b_path)
+    else:
+        raise ValueError(f"Invalid base model: {config.base_model}")
+
+    ar_model, train_scheduler = equip_internvl(ar_model, config.model)
 
 
 
 if __name__ == "__main__":
-    dev()
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="config/internvl/sandwich.yaml")
+    args = parser.parse_args()
+    main(args)
 
 
 
