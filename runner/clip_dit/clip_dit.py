10bad906508917b4fb96ad199a0e28b35b17524b
Jiachun Jin <jiachun22.jin@gmail.com>
2025-08-14 09:01:17 +0000

    sync

--- runner/clip_dit/clip_dit.py
+++ runner/clip_dit/clip_dit.py
@@ -230,6 +230,20 @@ def main(args):
                     accelerator.log(logs, step=global_step)
                     progress_bar.set_postfix(**logs)
 
+                    if global_step > 0 and global_step % config.train.save_every == 0 and accelerator.is_main_process:
+                        dit_model.eval()
+                        state_dict = accelerator.unwrap_model(dit_model).state_dict()
+                        save_path = os.path.join(output_dir, f"dit-{config.train.exp_name}-{global_step}")
+                        torch.save(state_dict, save_path)
+                        print(f"dit saved to {save_path}")
+
+                    accelerator.wait_for_everyone()
+
+        epoch += 1
+        accelerator.print(f"epoch {epoch}: finished")
+        accelerator.log({"epoch": epoch}, step=global_step)
+
+    accelerator.end_training()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
