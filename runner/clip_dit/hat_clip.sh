b036e798fd84afe625da0069fb878d74a2dd4a08
Jiachun Jin <jiachun22.jin@gmail.com>
2025-08-26 11:16:20 +0000

    sync

---
+++ runner/clip_dit/hat_clip.sh
@@ -0,0 +1,6 @@
+accelerate launch \
+--config_file config/accelerate_config/deepspeed \
+--main_process_port 30002 \
+--num_processes 8 \
+runner/clip_dit/hat_clip.py \
+--config config/clip_dit/hat_clip.yaml
