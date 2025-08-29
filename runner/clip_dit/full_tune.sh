56941d77948064450523be5cd95b61dc3ae5e7aa
Jiachun Jin <jiachun22.jin@gmail.com>
2025-08-29 12:01:07 +0800

    sync

--- runner/clip_dit/full_tune.sh
+++ runner/clip_dit/full_tune.sh
@@ -2,5 +2,5 @@ accelerate launch \
 --config_file config/accelerate_config/deepspeed \
 --main_process_port 30002 \
 --num_processes 8 \
-runner/clip_dit/fune_tune.py \
---config config/clip_dit/fune_tune.yaml
+runner/clip_dit/full_tune.py \
+--config config/clip_dit/full_tune.yaml
