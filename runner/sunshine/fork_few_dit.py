7125cb2ce05e1668efd234c699d4deb3e4996f97
Jiachun Jin <jiachun22.jin@gmail.com>
2025-09-03 16:16:02 +0800

    sync

--- runner/sunshine/fork_few_dit.py
+++ runner/sunshine/fork_few_dit.py
@@ -34,9 +34,128 @@ def main(args):
     internvl = InternVLChatModel.from_pretrained(config.model.internvl_path)
     internvl, train_scheduler = intern_to_fork(internvl, config.model)
 
+    vae_aligner = get_vae_aligner(config.vae_aligner)
+    ckpt = torch.load(config.vae_aligner.ckpt_path, map_location="cpu", weights_only=True)
+    vae_aligner.load_state_dict(ckpt, strict=True)
+    feature_down_projector = vae_aligner.siglip_feature_proj
+
+    global_step = config.train.global_step if config.train.global_step is not None else 0
     params_to_learn = list(p for p in internvl.parameters() if p.requires_grad)
 
+    optimizer = torch.optim.AdamW(
+        params_to_learn,
+        lr           = config.train.lr,
+        betas        = (0.9, 0.95),
+        weight_decay = 5e-2,
+        eps          = 1e-8,
+    )
+
+    if accelerator.mixed_precision == "bf16":
+        dtype = torch.bfloat16
+    elif accelerator.mixed_precision == "fp16":
+        dtype = torch.float16
+    else:
+        dtype = torch.float32
+
+    dataloader = get_t2i_dataloader(config.data, accelerator)
+    internvl, optimizer = accelerator.prepare(internvl, optimizer)
+    feature_down_projector.requires_grad_(False)
+    feature_down_projector = feature_down_projector.to(accelerator.device, dtype).eval()
+
+    training_done = False
+    epoch = 0
+    progress_bar = tqdm(
+        total   = config.train.num_iter,
+        initial = global_step,
+        desc    = "Steps",
+        disable = not accelerator.is_local_main_process,
+    )
+
+    config.device_count = accelerator.num_processes
+    if accelerator.is_main_process:
+        accelerator.init_trackers(config.train.wandb_proj, config=flatten_dict(config))
+        with open(os.path.join(output_dir, "config.yaml"), "w") as f:
+            OmegaConf.save(config, f)
+
     accelerator.print(f"Learnable parameters: {sum(p.numel() for p in params_to_learn if p.requires_grad) / 1e6} M")
+    accelerator.print(f"Accelerator mixed precision: {accelerator.mixed_precision}")
+
+    imagenet_mean = torch.tensor(IMAGENET_MEAN, device=accelerator.device, dtype=dtype).view(1, 3, 1, 1)
+    imagenet_std = torch.tensor(IMAGENET_STD, device=accelerator.device, dtype=dtype).view(1, 3, 1, 1)
+
+    while not training_done:
+        for batch in dataloader:
+            with accelerator.accumulate([internvl]):
+                internvl.train()
+                pixel_values = batch["pixel_values"].to(accelerator.device, dtype)
+                input_ids = batch["input_ids"].to(accelerator.device)
+                attention_mask = batch["attention_mask"].to(accelerator.device)
+
+                x_intern = (pixel_values - imagenet_mean) / imagenet_std
+                x_vae = pixel_values * 2 - 1
+
+                with torch.no_grad():
+                    x_clip = extract_feature_pre_adapter(internvl.vision_model, x_intern)
+                    visual_gen_feature = feature_down_projector(x_clip)
+
+                # ----- compute AR loss -----
+                B, L = input_ids.shape
+                text_embedding = internvl.language_model.get_input_embeddings()(input_ids).clone()
+                img_embedding = internvl.clip_projector(visual_gen_feature)
+                joint_embedding = torch.cat((text_embedding, img_embedding), dim=1)
+                img_mask = torch.ones((B, config.data.num_img_token), dtype=torch.bool, device=accelerator.device)
+                attention_mask = torch.cat([attention_mask, img_mask], dim=1)
+
+                hidden_states = internvl.language_model(
+                    inputs_embeds        = joint_embedding,
+                    attention_mask       = attention_mask,
+                    output_hidden_states = True,
+                ).hidden_states[-1]
+                hidden_state = hidden_states[:, -config.data.num_img_token-1:-1, :]
+
+                z = rearrange(hidden_state, "B L D -> (B L) D")
+                gt_feature = rearrange(visual_gen_feature, "B L D -> (B L) D")
+                timesteps = torch.randint(0, 1000, (z.shape[0],), dtype=torch.int64, device=z.device)
+                noise = torch.randn_like(gt_feature, device=z.device, dtype=z.dtype)
+                noisy_latents = train_scheduler.add_noise(gt_feature, noise, timesteps)
+                target = train_scheduler.get_velocity(gt_feature, noise, timesteps)
+                pred = internvl.diff_head(noisy_latents, timesteps, z)
+
+                loss_ar = torch.nn.functional.mse_loss(pred.to(dtype), target)
+
+                loss = loss_ar
+
+                accelerator.backward(loss)
+
+                if accelerator.sync_gradients:
+                    accelerator.clip_grad_norm_(params_to_learn, 1.0)
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+                    global_step += 1
+                    progress_bar.update(1)
+
+                    logs = dict(
+                        ar_loss = accelerator.gather(loss_ar.detach()).mean().item(),
+                    )
+                    accelerator.log(logs, step=global_step)
+                    progress_bar.set_postfix(**logs)
+
+                    if global_step > 0 and global_step % config.train.save_every == 0 and accelerator.is_main_process:
+                        internvl.eval()
+                        state_dict = accelerator.unwrap_model(internvl).state_dict()
+                        save_path = os.path.join(output_dir, f"internvl-{config.train.exp_name}-{global_step}")
+                        torch.save(state_dict, save_path)
+                        print(f"internvl saved to {save_path}")
+
+                    accelerator.wait_for_everyone()
+
+        epoch += 1
+        accelerator.print(f"epoch {epoch}: finished")
+        accelerator.log({"epoch": epoch}, step=global_step)
+
+    accelerator.end_training()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
